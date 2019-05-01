f900deccc9b5633287d3f0c8bbfdf7eea2dc2e08
Sylphria <azeroth13@gmail.com>
2019-05-01 15:21:08 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -49,7 +49,7 @@ function answer($text){
 		$regex = '/(ช่วยราชการ)/i';
 		if(preg_match($regex ,$text)){	
 		$hint = "กรณีมาช่วยราชการ ให้ทำผ่านระบบ Polis เข้ามา
-			\nวิธีการขอดังนี้ : https://drive.google.com/open?id=1CEfrynQpOygFCBhIlwr7ADuVZXW2twyz
+			\nวิธีการขอดังนี้ : https://drive.google.com/open?id=1m81LqrOcnGX8RpplxE0WeOK91Pxw7Lmh
 			\nหากพบปัญหาในการสมัครใช้งาน สามารถติดต่อได้ที่
 			\nLine ID : @citc.database";
 			break;
@@ -68,7 +68,7 @@ function answer($text){
 		if(preg_match($regex ,$text)){	
 			$hint = "การขอสิทธิ PTM ให้ทำผ่านระบบ Polis มา
 			\nโดยช่องปฏิบัติหน้าที่ให้กรอกว่า 'ใช้งานระบบ PTM'
-			\nวิธีการขอดังนี้ : https://drive.google.com/open?id=1CEfrynQpOygFCBhIlwr7ADuVZXW2twyz
+			\nวิธีการขอดังนี้ : https://drive.google.com/open?id=1m81LqrOcnGX8RpplxE0WeOK91Pxw7Lmh
 			\nหากเป็นเจ้าหน้าที่ ภจว. ทำการแจกจ่ายใบสั่งให้กดขอช่วยราชการที่ ภจว. ตนเองมาด้วย
 			\nสิทธิใบสั่งกล้องอนุมัติเฉพาะหน่วยงานที่ทำโครงการส่งมาแล้วเท่านั้น
 			\nกำหนดหลักเกณฑ์ใบสั่งกล้อง : https://drive.google.com/open?id=1_r35omdRiw1jsNW7M8LrPZfoyB3UZCvI
